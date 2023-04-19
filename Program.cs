af28793d318c45b63b3c2506f4b8157e6e4631b4
Alonso López <78806606+UsainBlitz@users.noreply.github.com>
2023-04-19 13:14:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Por favor, ingrese su nombre:");
+        var nombre = Console.ReadLine();
+
+        var nombreInvertido = "";
+        for (var i = nombre.Length - 1; i >= 0; i--)
+        {
+            nombreInvertido += nombre[i];
+        }
+
+        Console.WriteLine("¡Adiós " + nombreInvertido + "!");
+    }
+}
